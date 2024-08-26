82e90b10ce034a0fbf16dafcc8653ab9985a3b83
100791 <100791@MACTECH.LOC>
2024-08-26 09:03:28 +0530

    curren_location

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,57 +1,262 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather5/controller/weather_controller.dart';
 
-class Animated extends StatefulWidget {
-  const Animated({super.key});
+import 'package:weather5/utils/colors.dart';
+import 'package:weather5/utils/const.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:weather5/views/location_screen.dart';
+import 'package:weather5/views/setting_screen.dart';
+//import 'package:weather_location/utils/colors.dart';
+
+
+//import '../controller/weather_controller.dart';
+
+//import '../controller/weather_controller.dart';
+
+class home_screen extends StatefulWidget {
+  const home_screen({super.key});
 
   @override
-  State<Animated> createState() => _AnimatedState();
+  State<home_screen> createState() => _home_screenState();
 }
 
-class _AnimatedState extends State<Animated> {
-  double _rocketPosition = 400; // initial position of the rocket
-  double _objectPosition = 0; // initial position of the object
+class _home_screenState extends State<home_screen> {
+  //late WeatherController weatherController;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<WeatherController>().getCurrentWeather();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final weatherController =
+        Provider.of<WeatherController>(context);
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: _rocketPosition,
-            left: 150,
-            child: AnimatedContainer(
-              duration: Duration(seconds: 2),
-              curve: Curves.easeInOut,
-              child: Image.network(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8ZMe5LT-2tn4oeId8YVQ-XgQv1ETs_p3Z7A&s",
-                width: 100,
-                height: 100,
-              ),
-            ),
-          ),
-          Positioned(
-            top: _objectPosition,
-            left: 250,
-            child: AnimatedContainer(
-              duration: Duration(seconds: 2),
-              curve: Curves.bounceOut,
-              child: Container(
-                width: 50,
-                height: 50,
-                color: Colors.red,
-              ),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _rocketPosition = 0; // move the rocket to the top
-            _objectPosition = 400; // move the object to the bottom
-          });
-        },
-        child: Icon(Icons.play_arrow),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(),
+            kheight25,
+            weatherController.isLoding
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : weatherController.weatherResponseModel == null
+                    ? Center(
+                        child: Text("No Data Existing"),
+                      )
+                    : Container(
+                        padding: EdgeInsets.all(10),
+                        width: 358,
+                        height: 565,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [lightblue, darkblue],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    LocationScreen()));
+                                      },
+                                      icon: Icon(
+                                        Icons.add,
+                                        color: whitePrimary,
+                                        size: 32,
+                                      )),
+                                  // Icon(
+                                  //   Icons.add,
+                                  //   color: whitePrimary,
+                                  //   size: 32,
+                                  // ),
+                                  Text(
+                                    //"Malang",
+                                    weatherController
+                                        .weatherResponseModel!.name,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        color: whitePrimary,
+                                        fontSize: 16),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SettingScreen()));
+                                    },
+                                    icon: Icon(
+                                      Icons.more_vert_rounded,
+                                      color: whitePrimary,
+                                      size: 32,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              //Image.network("https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-cartoon-cloud-illustration-png-image_10211263.png"),
+                              Image.asset(
+                                  "assets/images/Sun cloud angled rain.png"),
+                              Text(
+                                "Sunday | Nov 14",
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                              Text(weatherController
+                                        .weatherResponseModel!.main.temp.toString(),
+                                  style: TextStyle(
+                                      fontSize: 72,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold)),
+                              Text(
+                                weatherController
+                                        .weatherResponseModel!.weather[0].main,
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.white),
+                              ),
+                              Divider(
+                                color: Colors.white,
+                              ),
+                              Row(
+                                children: [
+                                  Row(
+                                    children: [
+                                      kwidth25,
+                                      SvgPicture.asset(
+                                        "assets/svgs/location.svg",
+                                        width: 25,
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                      "${weatherController
+                                        .weatherResponseModel!.wind.speed.toString()}km/hr" ,
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                      Text(
+                                        "Wind",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 50),
+                                    child: Row(
+                                      children: [
+                                        kwidth25,
+                                        SvgPicture.asset(
+                                          "assets/svgs/Group.svg",
+                                          width: 25,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${weatherController.weatherResponseModel!.clouds.all}%",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                      Text(
+                                        "Clouds",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Row(
+                                children: [
+                                  Row(
+                                    children: [
+                                      kwidth25,
+                                      SvgPicture.asset(
+                                        "assets/svgs/temp.svg",
+                                        width: 25,
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${weatherController.weatherResponseModel!.main.pressure} mbar",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                      Text(
+                                        "Pressure",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 40),
+                                    child: Row(
+                                      children: [
+                                        kwidth25,
+                                        SvgPicture.asset(
+                                          "assets/svgs/hum.svg",
+                                          width: 25,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "${weatherController.weatherResponseModel!.main.humidity}",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                      Text(
+                                        "Humidity",
+                                        style: TextStyle(
+                                            fontSize: 12, color: whitePrimary),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+          ],
+        ),
       ),
     );
   }
